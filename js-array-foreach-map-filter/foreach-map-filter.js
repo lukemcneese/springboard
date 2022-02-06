b80d1b55de8f805f478ba515fb8bc98cb8d1664b
lukemcneese <lukemcneese@gmail.com>
2022-02-06 13:10:33 -0500

    someEvery

--- js-array-foreach-map-filter/foreach-map-filter.js
+++ js-array-foreach-map-filter/foreach-map-filter.js
@@ -45,6 +45,7 @@ function showFirstAndLast(arr){
     });
 }
 
+
 /*
 Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added for each object 
 
