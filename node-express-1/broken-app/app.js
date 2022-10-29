670e2447b5a2c03661b101f5b30ef71b2f45babe
lukemcneese <lukemcneese@gmail.com>
2022-10-29 14:49:45 -0400

    assessment 6

--- node-express-1/broken-app/app.js
+++ node-express-1/broken-app/app.js
@@ -1,18 +1,40 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
+app.use(express.json());
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
-  }
+app.post('/', async function(req, res, next)  {
+  let developers = req.body.developers;
+  const info = await getDevInfo(developers);
+  return res.json(info)
 });
 
-app.listen(3000);
+async function getDevInfo(developers){
+  let info = []
+  for(let dev of developers){
+    try{
+      const response = await axios.get(`https://api.github.com/users/${dev}`);
+      info.push({"name":response.data.name, "bio": response.data.bio});
+    }catch{
+        console.log("Couldn't find user")
+    }
+  }
+  return info
+};
+
+  // try {
+  //   let results = req.body.developers.map(async d => {
+  //     return await axios.get(`https://api.github.com/users/${d}`);
+  //   });
+  //   console.log(results)
+  //   //let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+  //   //return res.send(JSON.stringify(out));
+  // } catch (err) {
+  //   next(err);
+  // }
+
+
+app.listen(3000, function() {
+  console.log('Server started on port 3000.');
+});
--- node-express-1/broken-app/issues.md
+++ node-express-1/broken-app/issues.md
@@ -1,1 +1,6 @@
 # Broken App Issues
+made all imports const
+added app.use(express.json())
+added expressError custom Errors
+npm install express even after npm install
+added ther error at the beginning of the catch block
