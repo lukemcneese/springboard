e3847d50d2f552389fc82c4cc3d66cc30660b92e
lukemcneese <lukemcneese@gmail.com>
2022-12-04 14:37:34 -0500

    eightball

---
+++ react-eightball/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Eightball from './Eightball'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Eightball</h1>
+      <Eightball/>
+    </div>
+  );
+}
+
+export default App;
