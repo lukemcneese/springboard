b8628c9bb8990f229b6fb754d88e10ae1e51f590
lukemcneese <lukemcneese@gmail.com>
2023-02-04 12:22:12 -0500

    ratings backend finished

--- cocktail-creator/frontend/src/CocktailAPI.js
+++ cocktail-creator/frontend/src/CocktailAPI.js
@@ -92,9 +92,13 @@ class CocktailAPI {
     return res.user;
   }
 
+
+  /** Handles Rating API Calls */
+  static async createRating(username)
+
 }
 
-// for now, put token ("testuser" / "password" on class)
+//("testuser" / "password" on class)
 CocktailAPI.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
     "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
     "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
